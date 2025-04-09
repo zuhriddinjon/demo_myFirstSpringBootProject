de90ae75dd621944efc688d56fd15fdea09b4433
Zuhriddinjon <zuhriddinjonrayimjonov@gmail.com>
2025-04-09 16:09:08 +0500

    add jwt implementation

--- src/main/java/com/example/demo/service/UserService.java
+++ src/main/java/com/example/demo/service/UserService.java
@@ -7,7 +7,6 @@ import com.example.demo.model.User;
 import com.example.demo.repository.UserRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
