f50fa4835af5d1172b63e73eafed4992f291a413
Zuhriddinjon <zuhriddinjonrayimjonov@gmail.com>
2025-04-23 14:08:46 +0500

    file storage service optimization

--- src/main/java/com/example/demo/service/FileStorageService.java
+++ src/main/java/com/example/demo/service/FileStorageService.java
@@ -21,7 +21,7 @@ public class FileStorageService {
 
     private final String uploadDir;
 
-    public FileStorageService(@Value("${file.upload-dir:uploads/}") String uploadDir) {
+    public FileStorageService(@Value("${file.upload-dir}") String uploadDir) {
         this.uploadDir = uploadDir;
     }
 
